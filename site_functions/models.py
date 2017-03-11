e3c529a16ff6a4bd9f972db67661d03290614b59
Pedro Henrique <albpedro897@hotmail.com>
2017-03-11 03:13:20 -0300

    Add the file validation for the articles

--- site_functions/models.py
+++ site_functions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from validators import validate_article_type
 
 class Minicurso (models.Model):
 	mini_id = models.IntegerField(unique=True)
@@ -15,8 +16,8 @@ class Minicurso (models.Model):
 class UserProfile (models.Model):
 
 	MODALIDADE_CHOICES = (
-	('GRA', u'Estudante de Graduação'),
-	('PGR', u'Estudante de Pós-Graduação'),
+	('GRA', u'Estudante de Graduacao'),
+	('PGR', u'Estudante de Pos-Graduacao'),
 	('PRO', u'Profissional')
 ,)
 
@@ -24,16 +25,16 @@ class UserProfile (models.Model):
 	name = models.CharField(max_length=100)
 	instituicao = models.CharField(max_length=200)
 	cpf = models.CharField(max_length=11)
-	#validação de cpf usando a lib localflavors no forms.py
+	#validacao de cpf usando a lib localflavors no forms.py
 	phone = models.CharField(max_length=11)
-	#a validação do telefone é feita pelo localflavors no forms.py
+	#a validacao do telefone e feita pelo localflavors no forms.py
 	password = models.CharField(max_length=16)
-	#o password é setado como um campo de password no forms.py e não aqui
+	#o password e setado como um campo de password no forms.py e nao aqui
 	email = models.EmailField(max_length=254, unique=True)
 	comprovante = models.ImageField(upload_to='comprovantes/', default=False)
 	have_article = models.BooleanField(default=False)
 	had_paid = models.BooleanField(default=False)
-	
+
 	modalidade = models.CharField(
 		max_length=3,
 		choices=MODALIDADE_CHOICES,
@@ -46,7 +47,7 @@ class UserProfile (models.Model):
 class Article (models.Model):
 	user = models.ForeignKey(UserProfile,  on_delete=models.CASCADE, default=False)
 	title = models.CharField(max_length=100)
-	document = models.FileField(upload_to='articles/', default=False)
+	document = models.FileField(upload_to='articles/', default=False, validators=[validate_article_type])
 
 	def __str__(self):
 		return self.title
